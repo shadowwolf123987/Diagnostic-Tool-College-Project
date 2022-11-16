9d9f2a8f6e3d730d1ff1a1f6620d2aaae6c5bf7b
Oli <oliver.0t.taylor@gmail.com>
2022-11-16 12:11:58 +0000

    Done First question

--- Diagnostic_Tool_College_Project.py
+++ Diagnostic_Tool_College_Project.py
@@ -1,2 +1,19 @@
 
+print("Welcome to our diagnostics tools \n")
+error = (input("What is the issue you are facing? \n")).lower()
+print("\n")
 
+synonyms = ["application","app","software","applcation","applicaton","applcaton"]
+
+def mainFunc():
+    x=0
+    while x < len(synonyms)
+        if synonyms in error and "loading" in error or "load" in error and "not" in error or "wont" or "won't":
+            cableConnected = (input("Are you connected to your wifi? \n")).lower()
+            if cableConnected == "yes":
+                print("test")
+            elif cableConnected == "no":
+                print("Connect to your wifi")
+    x+=1
+
+mainFunc()
